460487f2a3758105c694ac0c22804b64db156f8d
Nycollas <nycollas.duaarte@hotmail.com>
2025-12-30 17:52:23 -0300

    update

--- upsell-1/script.js
+++ upsell-1/script.js
@@ -6,7 +6,6 @@ setInterval(() => {
   if (seconds <= 0) return;
 
   seconds--;
-
   const h = String(Math.floor(seconds / 3600)).padStart(2, "0");
   const m = String(Math.floor((seconds % 3600) / 60)).padStart(2, "0");
   const s = String(seconds % 60).padStart(2, "0");
